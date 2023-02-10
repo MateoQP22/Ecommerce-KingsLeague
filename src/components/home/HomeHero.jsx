e0165e87ef6ada2000ba607882bcbc5b71ff89de
MateoQP22 <73276257@continental.edu.pe>
2023-02-09 23:35:29 -0500

    home y estilos listos devAgustin

---
+++ src/components/home/HomeHero.jsx
@@ -0,0 +1,29 @@
+
+// import 'bootstrap-icons/font/bootstrap-icons.css';
+// import ecommerce1 from '../../assets/ecommerce1.jpg';
+const HomeHero = () => {
+  return (
+
+    <>
+      <section className='contenedor_hero'>
+        <div className='container'>
+          <div className='text_hero'>
+            <div className='title_hero'>
+              <h1 >Kings League</h1>
+            </div>
+            <div className='descripcion_hero'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere impedit molestias quia neque eum deserunt suscipit amet in doloribus
+                repudiandae labore, nihil cum ea sequi necessitatibus sapiente dolorem quibusdam. Non!</p>
+            </div>
+            <div className='img_hover'>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+
+      </section>
+    </>
+  )
+};
+
+export default HomeHero;
